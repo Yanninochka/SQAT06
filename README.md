9bafd4456a8fbe7998db42665cf19c741f137e2b
Yanninochka <pavlikyannina@gmail.com>
2018-05-01 19:30:05 -0400

    Assignment 1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SQAT06 - test
-yanna
+Steps:
+Download and Run applications: Node 7.8.0 version, GitHub, Visual Studio Code, and Selenium-standalone.
+Register a new account in GitHub 
+Look at GitHub page with the name “Izzat Bamieh”
+Install npm and clone "SQAT06" account to your own account.
+Open Visual Studio Code “SQAT06” file by selecting “Open Folder”
+Edit README.md file and save it
+Push into the Command prompt
+Click fork with name Izzat Bamieh  in Github. 
+After fork name Izzat Bamieh will change into own your name (Yanninochka). 
+Click "New pull request"create pull request
+ 
+
+
+
+
+
+
+
+
--- web_basic.js
+++ web_basic.js
@@ -5,10 +5,49 @@ const options = { desiredCapabilities: { browserName: 'chrome' } };
 
 const client = webdriverio.remote(options);
 
+
 client
     .init()
-    .url('https://duckduckgo.com/')
-    .setValue('#search_form_input_homepage', 'WebdriverIO')
+    .url('https://app.figure1.com/account/register')
+    .waitForVisible('.register-page__username-input', 5000)
+    .setValue('.register-page__username-input',Math.random().toString(36).substring(7))
+    .catch (function(err){
+        console.log(err)
+    })
+    .setValue('.register-page__email-input', Math.random().toString(36).substring(2,11) + '@gmail.com')
+    .catch (function(err){
+        console.log(err)
+    })
+    .setValue('.register-page__password-input ', '101010')
+    .catch (function(err){
+        console.log(err)
+    })
+    .setValue('.register-page__confirm-password-input', '101010')
+    .catch (function(err){
+        console.log(err)
+    })
+    .addValue('.register-page__specialties-list', 'Physician')
+    .catch (function(err){
+        console.log(err)
+    })
+    .addValue('.register-page__specialties-other-list', 'Cardiology')
+    .catch (function(err){
+        console.log(err)
+    })
+    .scroll('.register-page__terms-checkbox')
+    .catch (function(err){
+        console.log(err)
+    })
+    .click('.register-page__terms-checkbox')
+    .catch (function(err){
+        console.log(err)
+    })
+    .click('.register-page__submit-button')
+    .then(function(){
+      callback();
+    }).catch(function(error){
+    callback(error);
+  })
     .click('#search_button_homepage')
     .getTitle().then(function(title) {
         console.log('Title is: ' + title);
